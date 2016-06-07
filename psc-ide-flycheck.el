e1736e8632df25eb18fae15166bf8c617561dd70
Mario Rodas <marsam@users.noreply.github.com>
2016-06-06 23:57:04 -0500

    Insert psc suggestions in flycheck errors

--- psc-ide-flycheck.el
+++ psc-ide-flycheck.el
@@ -8,13 +8,20 @@
 
 ;;; Commentary:
 
-;;; Usage: (eval-after-load 'flycheck
-;;;          '(add-hook 'flycheck-mode-hook #'psc-ide-flycheck-setup))
+;; Usage:
+;;
+;;     (eval-after-load 'flycheck
+;;       '(add-hook 'flycheck-mode-hook #'psc-ide-flycheck-setup))
 
 ;;; Code:
 
-(require 'cl-lib)
+(eval-when-compile
+  (require 'cl-lib)
+  (require 'let-alist))
+
+(require 'seq)
 (require 'json)
+(require 'dash)
 (require 'flycheck)
 (require 'psc-ide-protocol)
 
@@ -24,72 +31,40 @@
   :tag "Flycheck PscIde Ignored Error Codes"
   :type '(repeat string))
 
-(defun psc-ide-flycheck-decode-purescript-error (checker type error)
-  (let* ((position (assoc 'position error))
-         (error-code (cdr (assoc 'errorCode error)))
-         ;; (end-line (cdr (assoc 'endLine position)))
-         ;; (end-col (cdr (assoc 'endColumn position)))
-         (err-msg (cdr (assoc 'message error)))
-         (filename (cdr (assoc 'filename error)))
-         (start-line (or (cdr (assoc 'startLine position)) 1))
-         (start-col (or (cdr (assoc 'startColumn position)) 1)))
-    (when (not (-contains? psc-ide-flycheck-ignored-error-codes error-code))
-      (flycheck-error-new-at start-line
-                             start-col
-                             type
-                             err-msg
-                             :id (concat filename
-                                         ":" (number-to-string start-line)
-                                         ":" (number-to-string start-col))
-                             :checker checker))))
-
-(defun psc-ide-flycheck-read-json (str)
-  (let* ((json-array-type 'list))
-    (condition-case nil
-        (json-read-from-string str)
-      (error nil))))
-
 (defun psc-ide-flycheck-parse-errors (data checker)
-  "Decode purescript json output errors."
-  (let* ((resultType (pcase (cdr (assoc 'resultType data))
-                         (`success 'warning)
-                         (_ 'error)))
-         (result (cdr (assoc 'result data)))
-         (errors (-filter #'identity
-                          (mapcar
-                           (lambda (x) (psc-ide-flycheck-decode-purescript-error checker resultType x))
-                           result))))
-         errors))
+  "Decode purescript json output errors from DATA with CHECKER."
+  (let (errors)
+    (let-alist data
+      (seq-do (lambda (err)
+                (let-alist err
+                  (unless (member .errorCode psc-ide-flycheck-ignored-error-codes)
+                    ;; HACK: for insert suggestions
+                    (put-text-property 0 1 :suggestion .suggestion .errorCode)
+                    (put-text-property 0 1 :endLine .position.endLine .errorCode)
+                    (put-text-property 0 1 :endColumn .position.endColumn .errorCode)
+                    (when .suggestion
+                      (setq .message (concat .message " ●")))
+                    (push (flycheck-error-new-at
+                           .position.startLine
+                           .position.startColumn
+                           'error
+                           .message
+                           :id .errorCode
+                           :checker checker
+                           :filename .filename)
+                          errors))))
+              .result))
+    errors))
 
-(defun psc-ide-flycheck-save-suggestions (errs)
-  (setq-local
-   psc-ide-flycheck-suggestions
-   (-map
-    (lambda (err)
-      (let* ((err-filename (cdr (assoc 'filename err)))
-             (err-position (cdr (assoc 'position err)))
-             (err-line (cdr (assoc 'startLine err-position)))
-             (err-column (cdr (assoc 'startColumn err-position)))
-             (err-id (concat err-filename ":" (number-to-string err-line)
-                             ":" (number-to-string err-column))))
-        (cons err-id err)))
-    (-filter (lambda (i) (and (cdr (assoc 'position i))
-                              (cdr (assoc 'suggestion i))))
-             errs))))
-
+;;;###autoload
 (defun psc-ide-flycheck-insert-suggestion ()
+  "Replace error with suggestion from psc compiler."
   (interactive)
-  (let* ((id (flycheck-error-id (car (flycheck-overlay-errors-at (point)))))
-          (err (cdr (assoc id psc-ide-flycheck-suggestions)))
-          (pos (cdr (assoc 'position err)))
-          (sugg (cdr (assoc 'suggestion err))))
-    (if (and pos sugg)
-        (let* ((start (save-excursion
-                        (goto-char (point-min))
-                        (forward-line (- (cdr (assoc 'startLine pos)) 1))
-                        (move-to-column (- (cdr (assoc 'startColumn pos)) 1))
-                        (point)))
-                (end (save-excursion
+  (-if-let* ((flycheck-err (car (flycheck-overlay-errors-at (point))))
+             (suggestion   (get-text-property 0 :suggestion (flycheck-error-id flycheck-err)))
+             (endLine      (get-text-property 0 :endLine    (flycheck-error-id flycheck-err)))
+             (endColumn    (get-text-property 0 :endColumn  (flycheck-error-id flycheck-err))))
+      (let* ((start (save-excursion
                       (goto-char (point-min))
                       (forward-line (- (flycheck-error-line flycheck-err) 1))
                       (move-to-column (- (flycheck-error-column flycheck-err) 1))
@@ -125,7 +100,6 @@ CALLBACK is the status callback passed by flycheck."
                 (lambda (result)
                   (condition-case err
                       (progn
-                        (psc-ide-flycheck-save-suggestions (append (cdr (assoc 'result result)) nil))
                         (let ((errors (psc-ide-flycheck-parse-errors result checker)))
                           (funcall callback 'finished errors)))
                     (`(error debug)
