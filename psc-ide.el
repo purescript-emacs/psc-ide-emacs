90b4a4fee13d37abb0518baa11ee8fdb531f7964
Cmdv <vincent@cmdv.me>
2019-12-11 16:44:54 +0000

    rename js to corefn

--- psc-ide.el
+++ psc-ide.el
@@ -164,7 +164,7 @@ files."
   :type '(repeat string))
 
 (defcustom psc-ide-codegen
-  '("js")
+  '("corefn")
   "Codegen used for compilation.
 Specified the codegen targets the rebuild should produce.
 Uses the same target names as the command line compiler. Defaults to just JS output"
